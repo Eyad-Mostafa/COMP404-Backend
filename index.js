4c2a454d51f7e3a1551005df6af63a14493bac91
Eyad Mostafa <your.email@example.com>
2026-05-05 22:18:17 +0300

    Fix typo

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import bootstrap from "./Src/index.router.js";
 import dotenv from "dotenv";
+import { errorMiddleware } from "./Src/middleware/error.middleware.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(errorMiddleware);
-
 await bootstrap(app, express);
+
+app.use(errorMiddleware);
 
 app.listen(port, "0.0.0.0", () => {
   console.log(` Server is running on Port ${port}`);
